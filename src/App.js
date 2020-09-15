eb7078acdb9117ef00530d59107e600fff793a64
SaxarKompot <saxarkompot@gmail.com>
2020-09-15 16:29:11 +0300

    brg-4 Get styles of react components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import Radium, { StyleRoot } from 'radium';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -46,16 +46,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    };
 
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -72,33 +65,26 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = "red";
-      // style[':hover'] = {
-      //   backgroundColor: "lightred",
-      //   color: "black",
-      //   transition: '1s'
-      // };
+      btnClass = classes.Red;
     }
 
-    let classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     };
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     };
 
     return (
-      // <StyleRoot>
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm React App!</h1>
-        <p className={classes.join(' ')}>This is really working</p>
+        <p className={assignedClasses.join(' ')}>This is really working</p>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
-      // </StyleRoot>
     );
   }
 }
