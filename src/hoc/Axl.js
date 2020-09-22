6deda86bdc36e39406077f246ae19ac261ec9df2
SaxarKompot <saxarkompot@gmail.com>
2020-09-22 12:21:05 +0300

    brg-7 Remove all unnecessary code from the project

--- src/hoc/Axl.js
+++
@@ -1,3 +0,0 @@
-const axl = (props) => props.children;
-
-export default axl; 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.module.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
    //<React.StrictMode>
-      <App title="Relevant Persons"/>,
+      <App />,
    //</React.StrictMode>,
    document.getElementById('root')
 );
