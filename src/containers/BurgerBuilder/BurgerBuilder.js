78402bab3b8dc0f741bb75c475edab0ae6176063
SaxarKompot <saxarkompot@gmail.com>
2020-09-30 17:13:04 +0300

    brg-7 Add BuildControls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import Auxiliary from "../../hoc/Auxiliary"
 import Burger from "../../components/Layout/Burger/Burger";
+import BuildControls from "../../components/Layout/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
+   // constructor(props){
+   //    super(props);
+   //    this.state = {...}
+   // }
+   state = {
+      ingredients: {
+         salad: 1,
+         bacon: 1,
+         cheese: 2,
+         meat: 2
+      }
+   }
+
    render() {
       return (
          <Auxiliary>
-            <Burger />
-            <div>Build Control</div>
+            <Burger ingredients={this.state.ingredients} />
+            <BuildControls />
          </Auxiliary>
       );
    }
